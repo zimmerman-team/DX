6d8e4e70742950192986636e6f1368373cbe0931
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-09-13 10:49:38 +0200

    chore: change build script to only build, not run after building

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ fi
 # Start
 # Check if an argument is provided
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 [dev|test|staging|prod]"
+  echo "Usage: $0 bash $0 [dev|test|staging|prod] [service name (optional) (up to 5 service names)]"
   exit 1
 fi
 # Extract the first argument provided
@@ -33,22 +33,22 @@ I5="$6"
 if [ "$MODE" = "dev" ]; then
   sudo rm .env
   sudo ln .env.dev .env
-  sudo docker compose -f docker-compose.dev.yml up --build $I1 $I2 $I3 $I4 $I5
+  sudo docker compose -f docker-compose.dev.yml build $I1 $I2 $I3 $I4 $I5
 elif [ "$MODE" = "prod" ]; then
   sudo rm .env
   sudo ln .env.prod .env
-  sudo docker compose up -d --build $I1 $I2 $I3 $I4 $I5
+  sudo docker compose build $I1 $I2 $I3 $I4 $I5
 elif [ "$MODE" = "staging" ]; then
   sudo rm .env
   sudo ln .env.staging .env
-  sudo docker compose -f docker-compose.staging.yml up -d --build $I1 $I2 $I3 $I4 $I5
+  sudo docker compose -f docker-compose.staging.yml build $I1 $I2 $I3 $I4 $I5
 elif [ "$MODE" = "test" ]; then
   sudo rm .env
   sudo ln .env.test .env
-  sudo docker compose -f docker-compose.test.yml up -d --build $I1 $I2 $I3 $I4 $I5
+  sudo docker compose -f docker-compose.test.yml build $I1 $I2 $I3 $I4 $I5
 else
   echo "Invalid mode. Use 'dev', 'test', 'staging' or 'prod'."
   exit 1
 fi
 
-echo "Build script is done."
+echo "Build script is done. You can now start the services using the 'scripts/start.sh $MODE' script."
